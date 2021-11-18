76bd74e77db802066d223b0559ffcd74b3dfa2c1
peimiaochien <r04633023@g.ntu.edu.tw>
2021-11-18 11:02:13 +0800

    add if else to check if file exist

--- git_data_test.sh
+++ git_data_test.sh
@@ -4,8 +4,8 @@
 #SBATCH -p ngs12G           # Partition Name 等同PBS裡面的 -q Queue name
 #SBATCH -c 3               # 使用的core數 請參考Queue資源設定 
 #SBATCH --mem=12G           # 使用的記憶體量 請參考Queue資源設定
-#SBATCH -o out.log          # Path to the standard output file 
-#SBATCH -e err.log          # Path to the standard error ouput file
+#SBATCH -o gitout.log          # Path to the standard output file 
+#SBATCH -e giterr.log          # Path to the standard error ouput file
 
 ref_genome_dir='/staging/reserve/paylong_ntu/AI_SHARE/reference/GATK_bundle/2.8/b37/'
 ref_genome_file='human_g1k_v37_decoy.fasta.gz'
@@ -20,18 +20,30 @@ export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 
-# reference genome preprocess
-#mkdir $HOME/.vep
-#mkdir $HOME/.vep/homo_sapiens
-#mkdir $HOME/.vep/homo_sapiens/102_GRCh37
+# reference genome preprocess, you can download on internet or make by your own.
+# reference file required .fa .fai .gzi
 
-#cd $HOME/.vep/homo_sapiens/102_GRCh37
-#rsync ${ref_genome_dir}/${ref_genome_file} -t ./
-#mv ${ref_genome_file} Homo_sapiens.GRCh37.dna.toplevel.fa.gz
+file1='Homo_sapiens.GRCh37.dna.toplevel.fa.gz'
+file2='Homo_sapiens.GRCh37.dna.toplevel.fa.gz.gzi'
+file3='Homo_sapiens.GRCh37.dna.toplevel.fa.gz.fai'
+file_path=$HOME/.vep/homo_sapiens/102_GRCh37
 
-#gunzip Homo_sapiens.GRCh37.dna.toplevel.fa.gz
-#bgzip -i Homo_sapiens.GRCh37.dna.toplevel.fa
-#samtools faidx Homo_sapiens.GRCh37.dna.toplevel.fa.gz
+
+if test -f "${file_path}/${file1}";then
+	echo "${file_path}/${file1} exist."
+else 
+	mkdir $HOME/.vep
+	mkdir $HOME/.vep/homo_sapiens
+	mkdir $HOME/.vep/homo_sapiens/102_GRCh37 
+	cd $HOME/.vep/homo_sapiens/102_GRCh37
+	rsync ${ref_genome_dir}/${ref_genome_file} -t ./
+	mv ${ref_genome_file} Homo_sapiens.GRCh37.dna.toplevel.fa.gz
+
+	gunzip Homo_sapiens.GRCh37.dna.toplevel.fa.gz
+	bgzip -i Homo_sapiens.GRCh37.dna.toplevel.fa
+	samtools faidx Homo_sapiens.GRCh37.dna.toplevel.fa.gz
+fi
+
 
 
 # vcf2maf
